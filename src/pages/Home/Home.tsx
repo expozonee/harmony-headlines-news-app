fdf4a4021e767400543b762fb365f8b162ab07df
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-27 20:41:17 +0200

    created the home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+import NewsCardList from "../../components/NewsCardList/NewsCardList";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <NewsCardList />
+    </main>
+  );
+}
