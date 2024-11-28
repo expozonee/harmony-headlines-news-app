ea5cc7041bf1a407a59dfdc7c208c942630d2b1c
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-28 18:26:20 +0200

    updated the prompt to work with the new slider added to select the mood desired to the news article

--- src/utils/moodHandle.ts
+++ src/utils/moodHandle.ts
@@ -17,9 +17,12 @@ export async function initialMoods(titles: string[]) {
   return JSON.parse(answer);
 }
 
-export async function changeMood(data: NewsData) {
-  const prompt = `i will give you a title and a news story. you need to 
-    make them both calmer, more empathetic and more positive - without loosing
+export async function changeMood(data: NewsData, valueMood: number) {
+  const prompt = `i will give you a title and a news story and the current mood level. you need to 
+    make them both calmer, more empathetic and more positive or more aggressive and charged according
+    to the desired mood level - the higher the mood the more aggressive and charged the title and the description to be
+    and the less the mood level make them both calmer, more empathetic and more positive.
+    the desired mood leve is: ${valueMood} - without loosing
     any of the important existing facts.  the title is: ${data.title}.
     The description is: ${data.description}.
     the previous mood rating to these data is (ranging from 0 to 10,
@@ -30,7 +33,7 @@ export async function changeMood(data: NewsData) {
         description: <new updated description here>
         mood: <new updated mood>
     }.
-    return ONLY this object, without any additional text or explanations.
+    return ONLY this object, without any additional text or explanations and always stick to the level of mood i provide.
     dont and anything before or after the {} of the object. dont return markdown.`;
   const result = await model.generateContent(prompt);
   const answer = result.response.text().trim();
