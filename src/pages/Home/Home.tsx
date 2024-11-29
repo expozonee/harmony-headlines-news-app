f712c6b522af2289ac43c2af9a84ee31479a36da
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-29 20:26:33 +0200

    changed the h1 text from 'Home' to 'News'

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import NewsCardList from "../../components/NewsCardList/NewsCardList";
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1>News</h1>
       <NewsCardList />
     </main>
   );
