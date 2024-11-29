0ed86bbb4305e2c92186cd85daa63e79777b10c4
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-29 20:50:54 +0200

    removed the duplicated main tag and removed the margin bottom on the h1 on the main page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,9 @@ import NewsCardList from "../../components/NewsCardList/NewsCardList";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>News</h1>
       <NewsCardList />
-    </main>
+    </>
   );
 }
