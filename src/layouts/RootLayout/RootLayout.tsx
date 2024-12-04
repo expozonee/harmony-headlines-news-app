89d675258458463748ebe3b931555a27fb0bd339
abrahem haj ali <boss.abrahem@hotmail.com>
2024-12-04 13:29:06 +0200

    added the button for sign in and moved the clerk provider to the root layout file

--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,21 +1,50 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import "./RootLayout.css";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 export default function RootLayout() {
+  const navigate = useNavigate();
   return (
-    <div className="layout__container">
-      <header>
-        <h1>Harmony Headlines News</h1>
-        <Link to={"/"}>
-          <button>News</button>
-        </Link>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <p>footer</p>
-      </footer>
-    </div>
+    <ClerkProvider
+      routerPush={(to: string) => navigate(to)}
+      routerReplace={(to: string) => navigate(to, { replace: true })}
+      publishableKey={PUBLISHABLE_KEY!}
+    >
+      <div className="layout__container">
+        <header>
+          <h1>Harmony Headlines News</h1>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <Link to={"/"}>
+              <button>News</button>
+            </Link>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <Link to={"/sign-in"}>
+                <button> Sign In</button>
+              </Link>
+            </SignedOut>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+        <footer>
+          <p>footer</p>
+        </footer>
+      </div>
+    </ClerkProvider>
   );
 }
